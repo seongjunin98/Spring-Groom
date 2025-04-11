6d21b182806ec5b3727bda61e3622f344f7e42cd
SeongJun98 <seongjunin98@gmail.com>
2025-04-11 17:42:49 +0900

    Study : bean

---
+++ chap04-bean/src/main/java/com/seongjun/section02/initdestroy/subsection01/java/Application.java
@@ -0,0 +1,45 @@
+package com.seongjun.section02.initdestroy.subsection01.java;
+
+import com.seongjun.common.Beverage;
+import com.seongjun.common.Bread;
+import com.seongjun.common.Product;
+import com.seongjun.common.ShoppingCart;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+//        String[] beanNames = context.getBeanDefinitionNames();
+//        for(String beanName : beanNames){
+//            System.out.println("beanName = " + beanName);
+//        }
+
+        Product carpBread = context.getBean("carpBread", Bread.class);
+        Product milk = context.getBean("milk", Beverage.class);
+        Product water = context.getBean("water", Beverage.class);
+
+        /* 첫 번째 손님이 쇼핑카트를 꺼낸다. */
+        ShoppingCart cart1 = context.getBean("cart", ShoppingCart.class);
+        cart1.addItem(carpBread);
+        cart1.addItem(milk);
+
+        System.out.println("cart1? : " + cart1.getItem());
+
+        /* 두 번째 손님 쇼핑카트 꺼낸다. */
+        ShoppingCart cart2 = context.getBean("cart", ShoppingCart.class);
+        cart2.addItem(water);
+
+        System.out.println("cart2? : " + cart2.getItem());
+
+        System.out.println(cart1.hashCode());
+        System.out.println(cart2.hashCode());
+
+        ((AnnotationConfigApplicationContext) context).close();  //컨테이너 강제 종료
+
+    }
+}
---
+++ chap04-bean/src/main/java/com/seongjun/section02/initdestroy/subsection01/java/ContextConfiguration.java
@@ -0,0 +1,45 @@
+package com.seongjun.section02.initdestroy.subsection01.java;
+
+import com.seongjun.common.Beverage;
+import com.seongjun.common.Bread;
+import com.seongjun.common.Product;
+import com.seongjun.common.ShoppingCart;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class ContextConfiguration {
+
+    @Bean
+    public Product carpBread(){
+
+        return new Bread("붕어빵", 1000, new java.util.Date());
+    }
+
+    @Bean
+    public Product milk(){
+
+        return new Beverage("딸기우유", 1500, 500);
+
+    }
+
+    @Bean
+    public Product water(){
+
+        return new Beverage("지리산암반수", 3000, 500);
+    }
+
+    @Bean
+    @Scope("prototype")
+    public ShoppingCart cart(){
+
+        return new ShoppingCart();
+    }
+
+    @Bean(initMethod = "openShop", destroyMethod = "closeShop")
+    public Owner owner(){
+
+        return new Owner();
+    }
+}
