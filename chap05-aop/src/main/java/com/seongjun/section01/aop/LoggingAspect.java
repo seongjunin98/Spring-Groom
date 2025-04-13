cb3843e551bc38125f7f227a4ed4a5139b8f3c03
SeongJun98 <seongjunin98@gmail.com>
2025-04-13 15:16:55 +0900

    Study : aop

--- chap05-aop/src/main/java/com/seongjun/section01/aop/LoggingAspect.java
+++ chap05-aop/src/main/java/com/seongjun/section01/aop/LoggingAspect.java
@@ -1,4 +1,24 @@
 package com.seongjun.section01.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
-}
+
+        @Pointcut("execution(* com.seongjun.section01.aop.*Service.*(..))")
+        public void logPointcuy() {}
+
+        @Before("LoggingAspect.logPointcuy()")
+        public void logBefore(JoinPoint joinPoint){
+
+            System.out.println("Before joinPoint.getTarget() : " + joinPoint.getTarget());
+
+            System.out.println("Before joinPoint.getSignature() : " + joinPoint.getSignature());
+        }
+    }
+
