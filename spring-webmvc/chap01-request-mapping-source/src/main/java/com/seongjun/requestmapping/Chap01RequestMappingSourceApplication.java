dac9b2e06f4032a8846e6aab0a80832055d9b88d
SeongJun98 <seongjunin98@gmail.com>
2025-04-14 17:46:13 +0900

    Study : request mapping

---
+++ spring-webmvc/chap01-request-mapping-source/src/main/java/com/seongjun/requestmapping/Chap01RequestMappingSourceApplication.java
@@ -0,0 +1,13 @@
+package com.seongjun.requestmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01RequestMappingSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap01RequestMappingSourceApplication.class, args);
+    }
+
+}
---
+++ spring-webmvc/chap01-request-mapping-source/src/test/java/com/seongjun/requestmapping/Chap01RequestMappingSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongjun.requestmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01RequestMappingSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
