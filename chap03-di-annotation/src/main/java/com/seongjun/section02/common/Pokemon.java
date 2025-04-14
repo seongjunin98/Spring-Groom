f8b87dc6d404abbf99b8c0bd767d508bacb621eb
SeongJun98 <seongjunin98@gmail.com>
2025-04-14 16:06:31 +0900

    structure

--- chap03-di-annotation/src/main/java/com/seongjun/section02/common/Pokemon.java
+++
@@ -1,7 +0,0 @@
-package com.seongjun.section02.common;
-
-public interface Pokemon {
-
-    /* 공격하다 */
-    void attack();
-}
--- chap05-aop/src/main/java/com/seongjun/section01/aop/ContextConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.seongjun.section01.aop;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true) //aop 기능을 사용하기 위해 사용한 어노테이션, 성능을 좋게 하기위해 proxyTargetClass = true.
-public class ContextConfiguration {
-}
