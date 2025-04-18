6da72ffcf21f40100aebf0699cebb1b081bbf4ad
SeongJun98 <seongjunin98@gmail.com>
2025-04-18 23:01:42 +0900

    delete : mission folder

--- mission/todolist/src/main/java/com/example/todolist/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.repository;
-
-import com.example.todolist.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
