2388d998529aeb38ff124b5dfec3988c4f8b2142
SeongJun98 <seongjunin98@gmail.com>
2025-04-21 15:11:44 +0900

    study : exception-handler

--- spring-webmvc/chap04-exception-handler-source/src/main/java/com/seongjun/exceptionhandler/GlobalExceptionHandler.java
+++ spring-webmvc/chap04-exception-handler-source/src/main/java/com/seongjun/exceptionhandler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.seongjun.exceptionhandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/* 전역 예외 처리를 담당 */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,4 +14,13 @@ public class GlobalExceptionHandler {
 
         return "error/nullPointer";
     }
+
+    /* 상위 타입인 Exception을 이용하면 구체적으로 작성하지 않은 타입의 에러가 발생하더라도
+    *  처리가 가능하므로 default 처리 용도로 사용할 수 있다.*/
+    @ExceptionHandler(Exception.class)
+    public String defaultExceptionHandler(Exception exception) {
+
+        return "error/default";
+    }
+
 }
--- spring-webmvc/chap04-exception-handler-source/src/main/java/com/seongjun/exceptionhandler/OtherController.java
+++ spring-webmvc/chap04-exception-handler-source/src/main/java/com/seongjun/exceptionhandler/OtherController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ExceptionHandlerController {
+public class OtherController {
 
-    @GetMapping("controller-null")
-    public String nullPointerExceptionTest() {
+    @GetMapping("other-controller-null")
+    public String otherNullPointerException(){
 
         String str = null;
         System.out.println(str.charAt(0));
@@ -17,32 +17,32 @@ public class ExceptionHandlerController {
         return "/";
     }
 
-    @ExceptionHandler(NullPointerException.class)
-    public String nullPointExceptionHandler(NullPointerException exception) {
-
-        System.out.println("controller 레벨의 exception 처리");
-
-        return "error/nullPointer";
-    }
-
-    @GetMapping("controller-user")
-    public String userExceptionTest() throws MemberRegistException {
+    @GetMapping("other-controller-user")
+    public String otherUserExceptionTest() throws MemberRegistException {
 
         boolean check = true;
         if(check) {
             throw new MemberRegistException("회원을 받을 수 없습니다!");
-
         }
-            return "/";
+
+        return "/";
     }
 
     @ExceptionHandler(MemberRegistException.class)
     public String userExceptionHandler(Model model, MemberRegistException exception) {
 
-        System.out.println("controller 레벨의 exception 처리");
+        System.out.println("Global 레벨의 exception 처리");
         model.addAttribute("exception", exception);
 
         return "error/memberRegist";
     }
 
+    @GetMapping("other-controller-array")
+    public String otherArrayExceptionTest(){
+
+        double[] array = new double[0];
+        System.out.println(array[0]);
+
+        return "/";
+    }
 }
