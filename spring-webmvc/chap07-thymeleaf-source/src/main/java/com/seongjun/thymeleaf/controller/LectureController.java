e7f6e323a7e887a79ef2eed2b84e9e4dfee50229
SeongJun98 <seongjunin98@gmail.com>
2025-04-22 15:11:05 +0900

    study : thymeleaf

--- spring-webmvc/chap07-thymeleaf-source/src/main/java/com/seongjun/thymeleaf/controller/LectureController.java
+++ spring-webmvc/chap07-thymeleaf-source/src/main/java/com/seongjun/thymeleaf/controller/LectureController.java
@@ -1,10 +1,14 @@
 package com.seongjun.thymeleaf.controller;
 
 import com.seongjun.thymeleaf.model.dto.MemberDTO;
+import com.seongjun.thymeleaf.model.dto.SelectCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("lecture")
@@ -22,4 +26,35 @@ public class LectureController {
 
         return mv;
     }
+
+    @GetMapping("conditional")
+    public ModelAndView conditional(ModelAndView mv){
+
+
+        mv.addObject("num", 1);
+        mv.addObject("str", "바나나");
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+        memberList.add(new MemberDTO("유관순", 22, '여', "서울시 노원구"));
+        memberList.add(new MemberDTO("장보고", 40, '남', "서울시 종로구"));
+        memberList.add(new MemberDTO("신사임당", 30, '여', "서울시 성북구"));
+
+        mv.addObject("memberList", memberList);
+
+        mv.setViewName("/lecture/conditional");
+
+        return mv;
+    }
+
+    @GetMapping("etc")
+    public ModelAndView etc(ModelAndView mv){
+
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 3);
+
+        mv.addObject(selectCriteria);
+        return mv;
+    }
+
+
 }
